7003434e03e4b6baf80841714d368cf98b318395
Tyler Hall <0tillathehun0@gmail.com>
2020-08-31 13:59:43 -0400

    feat: add example of lazy loading

---
+++ README.md
@@ -0,0 +1,9 @@
+# GCP Playground
+
+## Getting Started
+
+`yarn` && `yarn serve`
+
+## Node Version
+
+Uses Node version `10`. Run `nvm use` to set the proper node version
--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -7,7 +7,18 @@ import { printCache } from './utils/printCache'
 // Show cache before importing functions
 functions.logger.log(printCache(require.cache))
 
+/**
+ * ! These modules are loaded and evaluated on each cold start of _any_ Cloud Function.
+ * ! this can impact cold start times, as cold start times will tend towards the sum of
+ * ! all Cloud Function cold start times.
+ *
+ * ! Instead, use global namespace sparingly, loading only dependencies shared by all Cloud Functions.
+ * ! Additionally, only load dependencies within each Cloud Function itself.
+ * ! See the "lazy import" below
+ */
+// * depends on `case` which is evaluated for every function
 import OnMessageWriteCreateSlug from './onMessageWriteCreateSlug'
+// * depends on `moment` which is evaluated for every function
 import OnMessageWriteUpdateTimestamp from './onFooWriteUpdateTimestamp'
 
 admin.initializeApp()
@@ -27,3 +38,12 @@ export const onMessageWriteCreateSlug = functions.firestore
 export const onFooWriteUpdateTimestamp = functions.firestore
   .document('/foo/{documentId}')
   .onCreate(onMessageWriteUpdateTimestampHandler.trigger)
+
+// lazy import
+export const onMessageWriteAddEmoji = functions.firestore
+  .document('/messages/{documentId}')
+  .onCreate(async (...args) => {
+    // * depends on `node-emoji` which is only evaluated for this function
+    const Clazz = (await import('./onMessageWriteAddEmoji')).OnBarWriteAddEmoji
+    return new Clazz().trigger(...args)
+  })
